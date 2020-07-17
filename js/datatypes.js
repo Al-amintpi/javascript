6440ebcb896ded707e9676930a4dc943b39ac535
Al-amintpi <alamincmt7418@gmail.com>
2020-07-17 23:53:00 +0600

    Javascript datatype, function,loop

---
+++ js/datatypes.js
@@ -0,0 +1,28 @@
+var a = 5;
+var b = 2;
+var str =" alamin";
+var c = a+b+str;
+var x = 1.2;
+var y = 2.1;
+console.log(c);
+console.log("float number add "+sum);
+console.log('----------------------------');
+
+var f = 4;
+var f1 = 2;
+var f2 = f*f1
+console.log(f+f1);
+console.log('integer and float division '+parseInt(f/2.1));
+console.log('multiplication'+f2);
+console.log('division '+f/f1);
+console.log('--------------------------');
+
+
+var a = 2;
+var b = 2;
+var c =3
+console.log(a==b); 
+console.log(a==c);
+
+
+
---
+++ js/prime.js
@@ -0,0 +1,11 @@
+var number = 9;
+for(var i=2; i<=number; i++){
+	if(number%i==0){
+		console.log('Not prime');
+	}
+	else{
+		console.log('prime');
+	}
+}
+
+ 
