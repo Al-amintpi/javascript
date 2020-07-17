339797175eb085ed1b807e14d3ae5aeadfc4049c
Al-amintpi <alamincmt7418@gmail.com>
2020-07-18 00:33:47 +0600

    Fibbonnaci add

--- js/fibonnaci.js
+++ js/fibonnaci.js
@@ -1,1 +1,14 @@
-alert('hi');
+var fibbonacci = function(n){
+	var first = 0;
+	var second = 1;
+	var sum = 1;
+	for(var i=1; i<=n; i++){
+		var sum = first + second;
+		first = second
+		second = sum
+	}
+
+	return sum;
+}
+
+console.log(fibbonacci(5))
